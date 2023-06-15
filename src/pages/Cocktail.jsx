df01d2a7fa72e30c3cfea65f0dec969ca0bffabf
DoychinAlexandrov <doychin_alexandrov@abv.bg>
2023-06-15 22:28:17 +0300

    update cocktail

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -25,6 +25,13 @@ const Cocktail = () => {
         strInstructions: instructions,
     } = singleDrink;
 
+    const validIngredients = Object.keys(singleDrink)
+        .filter(
+            (key) =>
+                key.startsWith("strIngredient") && singleDrink[key] !== null
+        )
+        .map((key) => singleDrink[key]);
+
     return (
         <Wrapper>
             <header>
@@ -51,6 +58,19 @@ const Cocktail = () => {
                     <p>
                         <span className="drink-data">glass : </span>
                         {glass}
+                    </p>
+                    <p>
+                        <span className="drink-data">ingredients :</span>
+                        {validIngredients.map((item, index) => {
+                            return (
+                                <span className="ing" key={item}>
+                                    {item}
+                                    {index < validIngredients.length - 1
+                                        ? ","
+                                        : ""}
+                                </span>
+                            );
+                        })}
                     </p>
                     <p>
                         <span className="drink-data">instructions : </span>
